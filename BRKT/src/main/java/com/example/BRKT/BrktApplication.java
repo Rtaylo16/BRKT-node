204b3675c5d6dc270c66750c30e8ef351f3d4349
Rtaylo16 <superbia@ymail.com>
2023-03-20 22:04:18 -0400

    Init committ

---
+++ BRKT/src/main/java/com/example/BRKT/BrktApplication.java
@@ -0,0 +1,13 @@
+package com.example.BRKT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrktApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrktApplication.class, args);
+	}
+
+}
---
+++ BRKT/src/test/java/com/example/BRKT/BrktApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BRKT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrktApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
